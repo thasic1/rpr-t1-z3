26571c3bb03e25a13ceb6a24f70fa1c8311cce6f
thasic1 <thasic1@etf.unsa.ba>
2020-10-26 18:52:47 +0100

    Dodavanje maina-a

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -3,6 +3,13 @@ package ba.unsa.etf.rpr;
 public class Main {
 
     public static void main(String[] args) {
-        
+        Sat s = new Sat(15,30,45);
+        s.Ispisi();
+        s.Sljedeci();
+        s.Ispisi();
+        s.pomjeriZa(-48);
+        s.Ispisi();
+        s.Postavi(0,0,0);
+        s.Ispisi();
     }
 }
